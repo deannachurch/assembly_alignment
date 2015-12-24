f480bdc6f3ae3846bca5bbaae7cf776f6f52eacb
deannachurch <deanna.church@gmail.com>
2015-12-24 11:27:05 -0700

    Initial read of alignment file working

--- assm_align.py
+++ assm_align.py
@@ -24,8 +24,55 @@ import datetime
 import yaml
 import errno
 
+
+def parseAlignReport(fi, assm_name):
+	#data structures
+	gap_no_hit = defaultdict(int)
+	ungap_no_hit = defaultdict(int)
+	no_hit_loc = defaultdict(list)
+	try:
+		with open(fi, 'r') as infile:
+			data=csv.reader(infile, delimiter="\t")
+			for line in data:
+				#skip empty lines
+				if (len(line)==0):
+					#skip blank lines
+					continue
+				elif (line[0].startswith("#")):
+					#skip header lines
+					continue
+				elif (len(line) == 1):
+					#metadata
+					if line[0].startswith('Query Assembly Name:'):
+						query_asm_name = line[0].split(':')[1].lstrip()
+						if not query_asm_name == assm_name:
+							logging.critical("Assembly name mismatch: %s\t%s" % (query_asm_name, assm_name))
+							sys.exit(3)
+					if line[0].startswith('Sequence Name:'):
+						seq_name = line[0].split(':')[1].lstrip()
+				else:
+					#data lines
+					#print line
+					data_type=line[0]
+					start = int(line[1])
+					end = int(line[2])
+					gap_len = int(line[3])
+					ungap_len = int(line[4])
+					loc="%s:%d-%d" % (seq_name, start, end)
+					if data_type == "NoHit":
+						gap_no_hit[seq_name] += gap_len
+						ungap_no_hit[seq_name] += ungap_len
+						no_hit_loc[seq_name].append(loc)
+				
+	except IOError:
+		logging.critical("Can't open %s" % fi)
+		sys.exit(2)
+
+	print gap_no_hit
+
 def parseSeqRep(fi, assm_name, assm_acc):
-	assm_list=[]
+	#parse the NCBI seq report to get names, roles, assm-units, etc
+	assm_list={}
 	try:
 		with open(fi, 'r') as infile:
 			data=csv.reader(infile, delimiter="\t")
@@ -41,10 +88,12 @@ def parseSeqRep(fi, assm_name, assm_acc):
 			 		rec.length=int(line[8])
 			 		rec.role=line[1]
 			 		rec.assm_unit=line[7]
-			 		assm_list.append(rec)
+			 		assm_list[line[0]]=rec
 
 	except IOError:
-		print "Can't open %s" % fi
+		logging.critical("Can't open %s" % fi)
+		sys.exit(1)
+
 	return assm_list
 
 class Seq(object):
@@ -56,6 +105,11 @@ class Seq(object):
 		self.length=0#sequence length
 		self.role=""#seq role ('assembled-molecule, 'alt-scaffold', 'unlocalized-scaffold', 'unplaced-scaffold')
 		self.assm_unit=""#assembly unit seq is in 
+
+	def set_nohit(self, length, loc_list):
+		self.no_hit_len=length
+		self.no_hit_list=loc_list
+
 
 def main():
 	#set up logging
@@ -78,12 +132,12 @@ def main():
 	assm1=cfg_dict['input_files']['assm1']
 	assm2=cfg_dict['input_files']['assm2']
 	##create sequence objects
-	assm1_list=[]
-	assm2_list=[]
-	assm1_list=parseSeqRep(assm1['seq_rpt'], assm1['name'], assm1['acc'])
-	logging.info("Read %s, sequences: %d" % (assm1['name'], len(assm1_list)))
-	assm2_list=parseSeqRep(assm2['seq_rpt'], assm2['name'], assm2['acc'])
-	logging.info("Read %s, sequences: %d" % (assm2['name'], len(assm2_list)))
+	assm1_dict=parseSeqRep(assm1['seq_rpt'], assm1['name'], assm1['acc'])
+	logging.info("Read %s, sequences: %d" % (assm1['name'], len(assm1_dict)))
+	assm2_dict=parseSeqRep(assm2['seq_rpt'], assm2['name'], assm2['acc'])
+	logging.info("Read %s, sequences: %d" % (assm2['name'], len(assm2_dict)))
+	##parse alignment report
+	parseAlignReport(assm1['align_rpt'], assm1['name'])
 
 	
 
